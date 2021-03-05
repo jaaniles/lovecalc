0caaa0771e579248d0a47f1ed12e44d9c8b8ba45
Aleksi Vuorio <aleksi.vuorio@yahoo.com>
2021-03-06 01:42:21 +0200

    Prevent refetch for queries on duplicate

--- src/providers/QueryCacheProvider.tsx
+++ src/providers/QueryCacheProvider.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      refetchInterval: 900000, // 15 min
+    },
+  },
+});
 
 export const QueryCacheProvider: FC = ({ children }) => (
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
