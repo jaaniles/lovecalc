2cb9011b5ff17b59ea71ef7e1cba39922c8183b7
Jaani Leskinen <jaani.leskinen@fraktio.fi>
2021-10-11 13:00:36 +0300

    use love

--- src/pagesWithContext/IndexPage/IndexPage.tsx
+++ src/pagesWithContext/IndexPage/IndexPage.tsx
@@ -1,31 +1,40 @@
 import React, { FC, useState } from "react";
 import CountUp from "react-countup";
 
+import { Button } from "~/atoms/Button";
 import { Column } from "~/atoms/Column";
 import { PageContent } from "~/atoms/PageContent";
 import { Row } from "~/atoms/Row";
 import { TextField } from "~/atoms/TextField";
+import { H1 } from "~/atoms/typography/H1";
+import { H3 } from "~/atoms/typography/H3";
+import { calculateLove } from "~/lovecalculator/levenshtein";
 
 export const IndexPage: FC = () => {
   const [name, setName] = useState("");
+  const [love, setLove] = useState(0);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    const perc = calculateLove(name, "Fraktio");
+    setLove(perc);
   };
 
   return (
     <PageContent>
       <Row>
         <Column alignCenter>
-          <p>
-            Hei
-            {` ${name || "rakas"} `}
-            anna nimi
-          </p>
+          <H3>LASKE RAKKAUDEN YHTEENSOPIVUUS FRAKTION KANSSA</H3>
           <TextField onChange={handleChange} id="name" />
+          <Button onClick={handleSubmit}>LASKE RAKKAUS</Button>
         </Column>
         <Column>
-          <CountUp start={0} end={100} duration={5} />
+          <H1>
+            <CountUp start={0} end={love} duration={5} />
+          </H1>
         </Column>
       </Row>
     </PageContent>
