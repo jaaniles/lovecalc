10d2e73b96f57ed4ab2bae4a828b8f73ed7582a8
Aleksi Vuorio <aleksi.vuorio@yahoo.com>
2021-10-11 12:24:03 +0300

    strain value

--- src/lovecalculator/lovecalculator.ts
+++ src/lovecalculator/lovecalculator.ts
@@ -1,4 +1,4 @@
-type LoveFN = () => number;
+type LoveFN = (params: { name: string }) => number;
 
 const loveFuncs: LoveFN[] = [];
 
@@ -6,13 +6,13 @@ const clamp = function(min: number, max: number, value: number): number {
   return Math.min(Math.max(value, min), max);
 }
 
-export const lovecalculator = (): {
+export const lovecalculator = (params: { name: string }): {
   lovePercentage: number;
 } => {
   let totalLoveCount = 0;
 
   for (const loveFunc of loveFuncs) {
-    const loveCount = loveFunc();
+    const loveCount = loveFunc({ name: params.name });
     totalLoveCount = totalLoveCount + clamp(0, 100, loveCount);
   }
 
